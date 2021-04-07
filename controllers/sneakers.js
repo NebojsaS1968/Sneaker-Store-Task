75ed396a7b74dc8d5541184e3c0957ee0d1739e8
NebojsaS1968 <savicnebojsa10@gmail.com>
2021-04-07 17:02:32 +0200

    controllers for routes

--- controllers/sneakers.js
+++ controllers/sneakers.js
@@ -1,8 +1,50 @@
-const getAllSneakers = (req, res, next) => {};
+const db = require("../db/index");
 
-const getSneakerHistory = (req, res, next) => {};
+const getAllSneakers = async (req, res, next) => {
+  console.log("Fetching data ...");
+  const result = await db.query("SELECT * FROM all_sneakers");
+
+  res.status(200).json({
+    status: 200,
+    results: result.rows.length,
+    data: {
+      sneakers: result.rows,
+    },
+  });
+};
+
+const getSneakerHistory = async (req, res, next) => {
+  console.log("Fetching data ...");
+  const result = await db.query("SELECT * FROM history");
+
+  res.status(200).json({
+    status: 200,
+    results: result.rows.length,
+    data: {
+      sneakers_history: result.rows,
+    },
+  });
+};
+
+const insertSneakersHistory = async (req, res, next) => {
+  const name = req.body.name;
+  const price = req.body.price;
+
+  const result = await db.query(
+    "INSERT INTO history (id, name, price) VALUES ($1, $2) RETURNING *",
+    [name, price]
+  );
+
+  res.status(201).json({
+    status: 201,
+    data: {
+      new_history: result.rows[0],
+    },
+  });
+};
 
 module.exports = {
   getAllSneakers,
   getSneakerHistory,
+  insertSneakersHistory,
 };
