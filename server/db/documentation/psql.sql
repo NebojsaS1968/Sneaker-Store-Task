d7afa9d4a149348c7958ba5fde5a22d58cf112b8
NebojsaS1968 <savicnebojsa10@gmail.com>
2021-04-09 19:02:19 +0200

    documenting work in the psql

--- server/db/documentation/psql.sql
+++ server/db/documentation/psql.sql
@@ -9,18 +9,20 @@ CREATE DATABASE sneakers;
 CREATE TABLE all_sneakers (
     id BIGSERIAL NOT NULL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    price BIGINT NOT NULL
+    price BIGINT NOT NULL,
+    img VARCHAR(200) NOT NULL
 );
 
 -- creating table for history sneakers
 CREATE TABLE history (
-    id BIGSERIAL NOT NULL PRIMARY KEY,
+    id INT NOT NULL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    price BIGINT NOT NULL
+    price BIGINT NOT NULL,
+    img VARCHAR(200) NOT NULL
 );
 
 -- inserting some sneakers data
-INSERT INTO all_sneakers (name, price) VALUES ('Nike Air Jordan', 20000);
-INSERT INTO all_sneakers (name, price) VALUES ('DEČIJE PATIKE NIKE COURT BOROUGH LOW', 3992);
-INSERT INTO all_sneakers (name, price) VALUES ('ŽENSKE PATIKE PUMA SOFTRIDE SOPHIA RECYCLED WNS', 6632);
-INSERT INTO all_sneakers (name, price) VALUES ('MUŠKE PATIKE PUMA FUTURE 6.4 IT', 3876);
+INSERT INTO all_sneakers (name, price, img) VALUES ('Nike Air Jordan', 20000, 'https://www.n-sport.net/UserFiles/products/big/08/01/decije-patike-nike-jordan-access-bg-AV7941-001.jpg');
+INSERT INTO all_sneakers (name, price, img) VALUES ('DEČIJE PATIKE NIKE COURT BOROUGH LOW', 3992, 'https://www.n-sport.net/UserFiles/products/big/03/25/decije-patike-nike-court-borough-low-gs--839985-001.jpg');
+INSERT INTO all_sneakers (name, price, img) VALUES ('ŽENSKE PATIKE PUMA SOFTRIDE SOPHIA RECYCLED WNS', 6632, 'https://www.n-sport.net/UserFiles/products/big/01/28/zenske-patike-puma-softride-sophia-recycled-wn-s-194862-02.jpg');
+INSERT INTO all_sneakers (name, price, img) VALUES ('MUŠKE PATIKE PUMA FUTURE 6.4 IT', 3876, 'https://www.n-sport.net/UserFiles/products/big/08/25/muske-patike-puma-future-6-4-it-106199-01.jpg');
