0529ae152a77301bc19b3e0efe60a11ba7511b78
NebojsaS1968 <savicnebojsa10@gmail.com>
2021-04-09 10:35:45 +0200

    installing cors

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
 
 const PORT = 5000 || process.env.PORT;
 const sneakers = require("./routes/sneakers");
-
-const app = express();
 
 app.use("/api/v1/sneakers", sneakers);
 
