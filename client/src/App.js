6693284d350b08d6c0cf1275665da8a851e461dd
NebojsaS1968 <savicnebojsa10@gmail.com>
2021-04-09 10:39:37 +0200

    wrapping entire app inside context provider

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,21 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import OrderHistory from "./routes/OrderHistory";
 import UserCheckout from "./routes/UserCheckout";
+import { SneakerContextProvider } from "./context/SneakerContext";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/checkout" component={UserCheckout} />
-          <Route exact path="/history" component={OrderHistory} />
-        </Switch>
-      </Router>
-    </div>
+    <SneakerContextProvider>
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/checkout" component={UserCheckout} />
+            <Route exact path="/history" component={OrderHistory} />
+          </Switch>
+        </Router>
+      </div>
+    </SneakerContextProvider>
   );
 }
 
