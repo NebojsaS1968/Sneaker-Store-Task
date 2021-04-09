663d4a37812cd98cf2642920bf050ac613bbfc0e
NebojsaS1968 <savicnebojsa10@gmail.com>
2021-04-09 10:53:37 +0200

    fetching data from the server, putting it in state

--- client/src/components/SneakerList.jsx
+++ client/src/components/SneakerList.jsx
@@ -1,12 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useContext } from "react";
 import SneakerAPI from "../apis/SneakerAPI";
+import { SneakerContext } from "../context/SneakerContext";
 
 const SneakerList = () => {
+  // destructuring values from context API using useContext hook
+  const { sneakers, setSneakers } = useContext(SneakerContext);
+
+  // useEffect hook is for fetching the data from the server as soon as the component mount on to the screen
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await SneakerAPI.get("/");
-        console.log(response);
+        setSneakers(response.data.data.sneakers);
       } catch (err) {
         console.log(err);
       }
