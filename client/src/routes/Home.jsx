c6f7ac2a7cc3fe8a461430a40b2d47615cbebe07
NebojsaS1968 <savicnebojsa10@gmail.com>
2021-04-09 10:40:27 +0200

    importing and displaying SneakerList component

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
+import SneakerList from "../components/SneakerList";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <SneakerList />
     </div>
   );
 };
