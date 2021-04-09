27b84436fa04edc25e0febd1e79da5f7173ff640
NebojsaS1968 <savicnebojsa10@gmail.com>
2021-04-09 09:30:05 +0200

    setting up react-router and 3 routes (pages)

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import OrderHistory from "./routes/OrderHistory";
+import UserCheckout from "./routes/UserCheckout";
+
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/checkout" component={UserCheckout} />
+          <Route exact path="/history" component={OrderHistory} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/routes/UserCheckout.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserCheckout = () => {
+  return <div>User Checkout</div>;
+};
+
+export default UserCheckout;
