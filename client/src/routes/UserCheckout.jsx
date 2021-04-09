03db8e9073a73d68ee5ce55009a11cc8c7fe72b6
NebojsaS1968 <savicnebojsa10@gmail.com>
2021-04-09 19:10:27 +0200

    user checkout page, with total price and user validation

--- client/src/routes/UserCheckout.jsx
+++ client/src/routes/UserCheckout.jsx
@@ -1,11 +1,91 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
 import Header from "../components/Header";
+import { useHistory } from "react-router-dom";
+import SneakerAPI from "../apis/SneakerAPI";
+import { SneakerContext } from "../context/SneakerContext";
 
 const UserCheckout = () => {
+  const history = useHistory();
+
+  const { cart, setCart } = useContext(SneakerContext);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await SneakerAPI.get("/cart");
+        setCart(response.data.data.sneakers_history);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // Calculate the price
+  const finalPrice = () => {
+    let result = 0;
+    for (let i = 0; i < cart.length; i++) {
+      result = result + Number(cart[i].price);
+    }
+    return result;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await SneakerAPI.delete("/deleteCart");
+    } catch (err) {
+      console.log(err);
+    }
+    history.push("/");
+  };
+
+  const handleBack = () => {
+    history.push("/");
+  };
+
   return (
     <div>
       <Header />
-      <p>User Checkout</p>
+      <div className="container">
+        <div className="cart">
+          <h4>Items for purchase:</h4>
+          {cart.map((item) => {
+            return (
+              <div className="cart-item p-4" key={item.id}>
+                <img src={item.img} alt="" />
+                <p>{item.name}</p>
+                <p>{item.price} din.</p>
+              </div>
+            );
+          })}
+          <h4>Final price is: {finalPrice()} din.</h4>
+        </div>
+        <form action="" onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input type="text" className="form-control" id="name" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="lastName">Last Name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="lastName"
+              required
+            />
+          </div>
+          <button className="btn btn-primary">Confirm</button>
+          <button
+            className="btn btn-secondary"
+            type="button"
+            onClick={handleBack}
+          >
+            Back
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
