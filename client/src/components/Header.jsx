bd4df17f0e1b72f105a61fb3a27d99c09850c161
NebojsaS1968 <savicnebojsa10@gmail.com>
2021-04-09 10:36:51 +0200

    changed class to className

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Header = () => {
   return (
     <header>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <button
-          class="navbar-toggler"
+          className="navbar-toggler"
           type="button"
           data-toggle="collapse"
           data-target="#navbarNavDropdown"
@@ -13,17 +13,17 @@ const Header = () => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul class="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" href="/">
-                Home <span class="sr-only">(current)</span>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link" href="/">
+                Home <span className="sr-only">(current)</span>
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/checkout">
+            <li className="nav-item">
+              <a className="nav-link" href="/checkout">
                 <img
                   src="https://cdn0.iconfinder.com/data/icons/money-banking-filled-outline/32/Money__Bank_icon_filled_line_style_32px_for_sale_shopping_bag_Purchase_E-Commerce_-512.png"
                   width="40"
@@ -32,8 +32,8 @@ const Header = () => {
                 ></img>
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/history">
+            <li className="nav-item">
+              <a className="nav-link" href="/history">
                 Order History
               </a>
             </li>
